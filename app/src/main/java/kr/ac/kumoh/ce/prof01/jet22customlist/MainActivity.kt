30e9cb48f4a71105b8f32ebf6af50e0fbaf942d4
devbwoh <bwoh@outlook.kr>
2024-02-03 08:08:04 +0900

    🎉 시작

---
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet22customlist/MainActivity.kt
@@ -0,0 +1,32 @@
+package kr.ac.kumoh.ce.prof01.jet22customlist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import kr.ac.kumoh.ce.prof01.jet22customlist.ui.theme.Jet22CustomListTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Jet22CustomListTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+
+    }
+}
